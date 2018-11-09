856fa91f2d116947223c7b6c9aec7212df1a6525
Jelena Jovanovic <jeljov@gmail.com>
2018-11-09 19:48:44 +0100

    Updated text of the tasks for lab6

--- lab6/lab6_flight.py
+++ lab6/lab6_flight.py
@@ -6,21 +6,26 @@
 # - operated_by - the company that operates the flight
 # - passengers - list of passengers, that is, instances of the Passenger class
 #
-# Use appropriate decorators to create get and set methods for the following attributes:
-# - departure - make it a private attribute and assure that it is a datetime object of the format:
-#   "%Y-%m-%d %H:%M"; consider defining this format as a class attribute (departure_format)
-# - flight_num - make it a private attribute and assure that the flight number consist of
-#   two letters followed by 3-4 digits
-# - passengers - make it a private attribute and assure that only objects of the type Passenger
-#   can be added to the passengers list; if None is passed to the setter, create an empty
-#   passengers list
-#
 # The Flight class should implement the following methods:
-# - constructor (__init__())
+# - constructor (__init__()) - only flight number and departure have to be specified
+#
+# - get and set methods (using appropriate decorators) for the following attributes:
+#   - departure - make it a private attribute and assure that it is a datetime object of the format:
+#     "%Y-%m-%d %H:%M"; consider defining this format as a class attribute (departure_format)
+#   - flight_num - make it a private attribute and assure that the flight number consist of
+#     two letters followed by 3-4 digits
+#   - passengers - make it a private attribute and assure that only objects of the type Passenger
+#     can be added to the passengers list; if None is passed to the setter, create an empty
+#     passengers list
+#
 # - method that returns a string representation of the given Flight object (__str__())
+#
 # - class method from_Frankfurt_by_Lufthansa() that creates flights that fly from Frankfurt and
-#   are operated by Lufthansa (alternative constructor)
+#   are operated by Lufthansa (alternative constructor); the method receives flight number and
+#   the scheduled departure date and time
+#
 # - a generator method that generates a sequence of passengers who have not yet checked in
+#
 # - a generator method that generates a sequence of candidate passengers for an upgrade to
 #   the business class; those are the passengers of the economy class whose air miles
 #   exceed the given threshold (input parameter) and who have checked in for the flight;
